fe53735cbdc5278b2c1d90f2f2051730e08b0b4c
David Guzman <drguzman511@gmail.com>
2023-09-16 02:06:47 +0000

    practica_bodega datos_sql

---
+++ scripts/ejemplo.sql
@@ -0,0 +1,20 @@
+use sakila;
+
+SELECT
+   c.customer_id,
+   c.first_name,
+   c.last_name,
+   a.address_id,
+   ci.city_id,
+   ci.city,
+   co.country_id,
+   co.country
+
+from customer as c
+   inner join address as a using (address_id)
+   inner join city as ci using (city_id)
+   inner join country as co using(country_id)
+
+
+limit 5
+
